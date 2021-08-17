5819db3ebe98f3e61f378ee21dac5298ea6ace1a
seoyoungsoo <iulove37@naver.com>
2021-08-18 00:16:03 +0900

    Feat: 데이터베이스 스키마 설계
    유저 관련 스키마 설계

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  id: {
+    type: String,
+    maxlength: 20,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
