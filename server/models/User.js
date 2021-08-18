f80b6390cf1938286832aefaefea993038a30934
seoyoungsoo <iulove37@naver.com>
2021-08-18 18:57:44 +0900

    Feat #server: 유저 관련 mongoose method 추가
    - 비밀번호 암호화
    - 로그인 시 비밀번호 확인
    - 토큰 생성
    - 토큰 확인

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const userSchema = Schema({
+const saltRounds = 10;
+
+const userSchema = mongoose.Schema({
   id: {
     type: String,
     maxlength: 20,
@@ -22,6 +25,57 @@ const userSchema = Schema({
     type: Number,
   },
 });
+
+// 비밀번호 암호화
+userSchema.pre('save', function (next) {
+  let user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+// 로그인 비밀번호 확인
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+// 토큰 생성
+userSchema.methods.generateToken = function (cb) {
+  let user = this;
+
+  const token = jwt.sign(user._id.toHexString(), 'accessToken');
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+// 토큰 확인
+userSchema.statics.findByToken = function (token, cb) {
+  let user = this;
+
+  jwt.verify(token, 'accessToken', function (err, decoded) {
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 
