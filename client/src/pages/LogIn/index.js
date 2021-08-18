9d4e1d017fc6c28d84febfc4f5efdc265443a231
seoyoungsoo <iulove37@naver.com>
2021-08-18 18:55:48 +0900

    Feat #client: 로그인 화면 렌더링

--- client/src/pages/LogIn/index.js
+++ client/src/pages/LogIn/index.js
@@ -1,7 +1,12 @@
+import SignUp from '@src/events/SignUp';
 import '@src/pages/LogIn/styles.css';
 
 class LogIn {
-  constructor() {}
+  constructor() {
+    this.eventTarget = document.querySelector('body');
+    this.events = new SignUp(this.eventTarget);
+    this.events.init();
+  }
 
   render() {
     document.body.innerHTML = `
@@ -10,11 +15,11 @@ class LogIn {
         <h2>LogIn</h2>
         <div class="login_id">
           <h4>ID</h4>
-          <input type="text" name="" id="" placeholder="Id">
+          <input type="text" name="id" id="id" placeholder="Id">
         </div>
         <div class="login_pw">
           <h4>Password</h4>
-          <input type="password" name="" id="" placeholder="Password">
+          <input type="password" name="password" id="password" placeholder="Password">
         </div>
         <div class="submit">
           <input type="submit" value="로그인">
