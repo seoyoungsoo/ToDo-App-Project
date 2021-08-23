47993b72e5922ce92fe09a5350e11e625cfbd8c0
seoyoungsoo <iulove37@naver.com>
2021-08-23 20:44:44 +0900

    Feat: 메인화면 헤더 추가

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,11 +1,18 @@
+import Header from '@src/components/Header';
+
 class Main {
   constructor() {
     this.element = document.createElement('div');
+    this.element.className = 'main-container';
+
+    this.header = new Header(this.element);
   }
 
   render() {
     document.body.appendChild(this.element);
-    this.element.innerHTML = `hi`;
+    this.element.innerHTML = `
+      ${this.header.render()}
+    `;
   }
 }
 
